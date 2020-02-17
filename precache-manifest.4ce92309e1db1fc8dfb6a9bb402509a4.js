04731757cd71b73659d5db49b3e9e820f8252431
Isidoros Kefakis <ikefakis@gmail.com>
2020-02-17 21:14:21 +0200

    Updates

---
+++ precache-manifest.4ce92309e1db1fc8dfb6a9bb402509a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe5d7824489086edc05ec4f7fce8383",
+    "url": "/react-polaroid-photo-deck/index.html"
+  },
+  {
+    "revision": "7a7380c6afa05269ad39",
+    "url": "/react-polaroid-photo-deck/static/css/main.9faa7e72.chunk.css"
+  },
+  {
+    "revision": "d3a33aac63889210fd4f",
+    "url": "/react-polaroid-photo-deck/static/js/2.7e458c1e.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/react-polaroid-photo-deck/static/js/2.7e458c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7380c6afa05269ad39",
+    "url": "/react-polaroid-photo-deck/static/js/main.73194c59.chunk.js"
+  },
+  {
+    "revision": "1a92664d7fec9bb77eb4",
+    "url": "/react-polaroid-photo-deck/static/js/runtime-main.d5a0729f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-polaroid-photo-deck/precache-manifest.4ce92309e1db1fc8dfb6a9bb402509a4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-polaroid-photo-deck/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
